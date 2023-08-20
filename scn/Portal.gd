01afa71e87b42ed9bf647ac74fe2944637f89de1
Utherbone <gabriel.saraiva.dev@outlook.com>
2023-08-20 02:11:14 -0400

    test

--- scn/Portal.gd
+++ scn/Portal.gd
@@ -32,6 +32,7 @@ func envelopar():
 
 
 func _on_Area2D_body_entered(body):
-	$AnimalSalvo.play()
-	emit_signal("resgatou_um_animal_com_o_portal")
+	#$AnimalSalvo.play()
+	#emit_signal("resgatou_um_animal_com_o_portal")
 	body.queue_free()
+
