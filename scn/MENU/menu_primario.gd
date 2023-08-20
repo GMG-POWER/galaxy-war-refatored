39fba2dbdc57430a56df73cc684ef61e8f3bd8db
Utherbone <gabriel.saraiva.dev@outlook.com>
2023-08-20 11:52:18 -0400

    update imenus (under work)

--- scn/MENU/menu_primario.gd
+++ scn/MENU/menu_primario.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
 
 
 
 func _on_jogar_pressed():
-	get_tree().change_scene_to_file("res://CENAS/modo de jogo/modo_de_jogo.tscn")
+	get_tree().change_scene_to_file("res://scn/MENU/modo_jogo/modo_jogo.tscn")
 
 
 
