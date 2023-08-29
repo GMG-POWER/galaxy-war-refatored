c0bf4512a3eafa0ce6febd4dbe59fea138e06dfa
Utherbone <gabriel.saraiva.dev@outlook.com>
2023-08-29 19:36:55 -0400

    Rebuild interface 1.0.0.1
    Rebuild interface and delete all implementation collision physics

--- scn/jogo/Cena_Principal.gd
+++ scn/jogo/Cena_Principal.gd
@@ -1,44 +1,57 @@
 extends Node2D
 
-var gerador = RandomNumberGenerator.new()
 var pontos = 0
 
 var instMeteoro = preload("res://scn/jogo/objetos/meteoro.tscn")
-#var instMissel = preload("res://scn/jogo/nave/lazer.tscn")
+var instShot = preload("res://scn/jogo/projetil/plasma_shot.tscn")
 var instSpace = preload("res://scn/jogo/bg_space/space.tscn")
-
+var cena_recarregada = preload("res://scn/jogo/projetil/plasma_shot.tscn").instantiate()
 
 func _ready():
+	$Plasma.queue_free()
 	$Timer.start()
-	gerador.randomize()
 	
 func criarAsteroid():
 	var instance
 	instance = instMeteoro.instantiate()
 	instance.position.y = 0
-	instance.position.x = 535
+	instance.position.x = 576
 	add_child(instance)
+	
+func AtirarPlasma():
+	if get_node_or_null(NodePath("Plasma")):
+		$Plasma.visible = 1
+		$Plasma.freeze = 0
 
 func _on_Timer_timeout():
 	$Timer.wait_time = 4.5
 	criarAsteroid()
 	$Timer.start()
 
-func _on_Portal_resgatou_um_animal_com_o_portal():
-	pontos += 1
-	$Label.text = str(pontos)
-
 func _on_Cena_Principal_draw():
-	$ambient.play()
-
-func __on_vector_resgatou_um_animal_com_o_portal():
-	pontos += 1
-	$Label.text = str(pontos)
-
-func _on_button_pressed():
+	#$ambient.play()
+	pass
 	
-	if $Label2.text == $TextEdit.text:
+func _on_nave_meteoro_atingi_nave():
+	pontos -= 1
+	$Label.text = str(pontos)
+	
+func _on_button_pressed():
+	if $Label2.text == $TextoOriginal.text:
 		pass
 	else:
 		print("As strings são diferentes.")
-	$TextEdit.text=""
+		
+func _on_btn_editar_texto_pressed():
+	if $Label2. == $TextoOriginal.text:
+		pass
+	else:
+		print("As strings são diferentes.")
+
+func _on_plasma_plasma_atinge_meteoro():
+	pontos += 1
+	$Label.text = str(pontos)
+	cena_recarregada.queue_free()
+	
+
+
--- scn/jogo/objetos/meteoro.gd
+++ scn/jogo/objetos/meteoro.gd
@@ -3,3 +3,6 @@ extends RigidBody2D
 func _process(_delta):
 	if position.y > 750:
 		queue_free()
+
+func _on_body_entered(body):
+	pass 
